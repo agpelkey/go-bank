2402460dcb56d3fa785699a8950f9e56e3fff33a
alex <agpelkey@yahoo.com>
2023-02-12 11:38:28 -0800

    adding storage interface and connection to Postgres container

--- api.go
+++ api.go
@@ -9,35 +9,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(v)
-}
-
-type apiFunc func(http.ResponseWriter, *http.Request) error // function signature we are using in this app
-
-type APIError struct {
-	Error string
-}
-
-// decorate out apiFunc into an http handlerfunc
-func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if err := f(w, r); err != nil {
-			// handle the error
-			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
-		}
-	}
-}
-
 type APIServer struct {
 	listneAddr string
+	store Storage
 }
 
-func NewAPIServer(listenAddr string) *APIServer {
+func NewAPIServer(listenAddr string, store Storage) *APIServer {
 	return &APIServer{
 		listneAddr: listenAddr,
+		store: store,
 	}
 
 }
@@ -89,4 +69,27 @@ func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) 
 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
 	return nil
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error // function signature we are using in this app
+
+type APIError struct {
+	Error string
+}
+
+// decorate out apiFunc into an http handlerfunc
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			// handle the error
+			WriteJSON(w, http.StatusBadRequest, APIError{Error: err.Error()})
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
-	server := NewAPIServer(":8080")
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+
+	server := NewAPIServer(":8080", store)
 
 	server.Run()
 }
